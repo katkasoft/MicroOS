0ecc9a938f84dd2e557476ae27170c0079d602b0
katkasoft <katkaleks@gmail.com>
2026-02-12 15:21:21 +0300

    syscall-lang interactive mode

--- bin/syscall-lang.cpp
+++ bin/syscall-lang.cpp
@@ -7,11 +7,100 @@
 
 using namespace std;
 
+void execute_line(string line) {
+    long arg1 = 0; string arg1_temp = ""; bool arg1_ready = false;
+    long arg2 = 0; string arg2_temp = ""; bool arg2_ready = false;
+    long arg3 = 0; string arg3_temp = ""; bool arg3_ready = false;
+    string arg3_string = ""; bool is_arg3_string = false;
+    long arg4 = 0; string arg4_temp = ""; bool arg4_ready = false;
+    bool quote_opened = false;
+    for (char c : line) {
+        if (c == '#' && !quote_opened) {
+            break;
+        }
+        if (c == ' ' && !quote_opened) {
+            if (!arg1_ready && !arg1_temp.empty()) {
+                arg1 = stol(arg1_temp);
+                arg1_ready = true;
+            } else if (arg1_ready && !arg2_ready && !arg2_temp.empty()) {
+                arg2 = stol(arg2_temp);
+                arg2_ready = true;
+            } else if (arg2_ready && !arg3_ready && (!arg3_temp.empty() || is_arg3_string)) {
+                if (!is_arg3_string) arg3 = stol(arg3_temp);
+                arg3_ready = true;
+            } else if (arg3_ready && !arg4_ready && !arg4_temp.empty()) {
+                arg4 = stol(arg4_temp);
+                arg4_ready = true;
+            }
+            continue;
+        }
+        if (!arg1_ready) {
+            if (isdigit(c)) {
+                arg1_temp += c;
+            } else if (!isspace(c)) {
+                cerr << "syscall-lang: error: first argument must be integer!" << endl;
+                exit(1);
+            }
+        } else if (!arg2_ready) {
+            if (isdigit(c)) {
+                arg2_temp += c;
+            } else if (!isspace(c)) {
+                cerr << "syscall-lang: error: second argument must be integer!" << endl;
+                exit(1);
+            }
+        } else if (!arg3_ready) {
+            if (c == '"') {
+                if (quote_opened) {
+                    arg3_string = arg3_temp;
+                    arg3_temp = "";
+                }
+                quote_opened = !quote_opened;
+                is_arg3_string = true;
+                continue;
+            }
+            arg3_temp += c;
+        } else if (!arg4_ready) {
+            if (isdigit(c)) {
+                arg4_temp += c;
+            } else if (!isspace(c)) {
+                cerr << "syscall-lang: error: fourth argument must be integer!" << endl;
+                exit(1);
+            }
+        }
+    }
+    if (!arg4_ready && !arg4_temp.empty()) {
+        arg4 = stol(arg4_temp);
+        arg4_ready = true;
+    }
+    if (arg1_ready && arg2_ready && (arg3_ready || is_arg3_string) && arg4_ready) {
+        long return_code;
+        if (is_arg3_string) {
+            string processed_str = "";
+            for (size_t i = 0; i < arg3_string.length(); ++i) {
+                if (arg3_string[i] == '\\' && i + 1 < arg3_string.length() && arg3_string[i+1] == 'n') {
+                    processed_str += '\n';
+                    i++;
+                } else {
+                    processed_str += arg3_string[i];
+                }
+            }
+            return_code = syscall(arg1, arg2, processed_str.c_str(), arg4);
+        } else {
+            return_code = syscall(arg1, arg2, arg3, arg4);
+        }
+        if (return_code == -1) {
+            cerr << "syscall-lang: error: " << line << " executed with error." << endl;
+        }
+    } else if (!line.empty() && line[0] != '#') {
+        cerr << "syscall-lang: error: must be four arguments in line" << endl;
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc == 2) {
         string arg1 = argv[1];
         if (arg1 == "--help" || arg1 == "help") {
-            std::cout << "MicroOS syscall-lang v0.1" << std::endl;
+            std::cout << "MicroOS syscall-lang v0.2" << std::endl;
             std::cout << "Usage: syscall-lang [file]" << std::endl;
             std::cout << "A low-level language for direct Linux system calls execution." << std::endl;
             std::cout << "Syntax:" << std::endl;
@@ -21,109 +110,31 @@ int main(int argc, char* argv[]) {
             std::cout << "  - Comments start with #." << std::endl;
             std::cout << "Example (Hello World):" << std::endl;
             std::cout << "  1 1 \"Hello, World!\\n\" 14" << std::endl;
+            return 0;
         }
         ifstream file(arg1);
         if (file.is_open()) {
             string line;
             while (getline(file, line)) {
-                long arg1 = 0; string arg1_temp = ""; bool arg1_ready = false;
-                long arg2 = 0; string arg2_temp = ""; bool arg2_ready = false;
-                long arg3 = 0; string arg3_temp = ""; bool arg3_ready = false;
-                string arg3_string = ""; bool is_arg3_string = false;
-                long arg4 = 0; string arg4_temp = ""; bool arg4_ready = false;
-                bool quote_opened = false;
-
-                for (char c : line) {
-                    if (c == '#' && !quote_opened) {
-                        break;
-                    }
-                    if (c == ' ' && !quote_opened) {
-                        if (!arg1_ready && !arg1_temp.empty()) {
-                            arg1 = stol(arg1_temp);
-                            arg1_ready = true;
-                        } else if (arg1_ready && !arg2_ready && !arg2_temp.empty()) {
-                            arg2 = stol(arg2_temp);
-                            arg2_ready = true;
-                        } else if (arg2_ready && !arg3_ready && (!arg3_temp.empty() || is_arg3_string)) {
-                            if (!is_arg3_string) arg3 = stol(arg3_temp);
-                            arg3_ready = true;
-                        } else if (arg3_ready && !arg4_ready && !arg4_temp.empty()) {
-                            arg4 = stol(arg4_temp);
-                            arg4_ready = true;
-                        }
-                        continue;
-                    }
-
-                    if (!arg1_ready) {
-                        if (isdigit(c)) {
-                            arg1_temp += c;
-                        } else if (!isspace(c)) {
-                            cerr << "syscall-lang: error: first argument must be integer!" << endl;
-                            return 1;
-                        }
-                    } else if (!arg2_ready) {
-                        if (isdigit(c)) {
-                            arg2_temp += c;
-                        } else if (!isspace(c)) {
-                            cerr << "syscall-lang: error: second argument must be integer!" << endl;
-                            return 1;
-                        }
-                    } else if (!arg3_ready) {
-                        if (c == '"') {
-                            if (quote_opened) {
-                                arg3_string = arg3_temp;
-                                arg3_temp = "";
-                            }
-                            quote_opened = !quote_opened;
-                            is_arg3_string = true;
-                            continue;
-                        }
-                        arg3_temp += c;
-                    } else if (!arg4_ready) {
-                        if (isdigit(c)) {
-                            arg4_temp += c;
-                        } else if (!isspace(c)) {
-                            cerr << "syscall-lang: error: fourth argument must be integer!" << endl;
-                            return 1;
-                        }
-                    }
-                }
-
-                if (!arg4_ready && !arg4_temp.empty()) {
-                    arg4 = stol(arg4_temp);
-                    arg4_ready = true;
-                }
-
-                if (arg1_ready && arg2_ready && (arg3_ready || is_arg3_string) && arg4_ready) {
-                    long return_code;
-                    if (is_arg3_string) {
-                        string processed_str = "";
-                        for (size_t i = 0; i < arg3_string.length(); ++i) {
-                            if (arg3_string[i] == '\\' && i + 1 < arg3_string.length() && arg3_string[i+1] == 'n') {
-                                processed_str += '\n';
-                                i++;
-                            } else {
-                                processed_str += arg3_string[i];
-                            }
-                        }
-                        return_code = syscall(arg1, arg2, processed_str.c_str(), arg4);
-                    } else {
-                        return_code = syscall(arg1, arg2, arg3, arg4);
-                    }
-                    if (return_code == -1) {
-                        cerr << "syscall-lang: error: " << line << " executed with error." << endl;
-                    }
-                } else if (!line.empty() && line[0] != '#') {
-                    cerr << "syscall-lang: error: must be four arguments in line" << endl;
-                }
+                execute_line(line);
             }
         } else {
             cerr << "syscall-lang: error while opening file " << arg1 << endl;
             return 1;
         }
     } else {
-        cout << "Usage: syscall-lang [file]" << endl;
-        return 1;
+        cout << "MicroOS syscall-lang v0.2" << endl;
+        cout << "Type exit to quit" << endl;
+        while (true) {
+            string line;
+            cout << "syscall> ";
+            if (!getline(cin, line) || line == "exit") {
+                return 0;
+            }
+            if (!line.empty()) {
+                execute_line(line);
+            }
+        }
     }
     return 0;
 }
