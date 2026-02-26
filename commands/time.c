9c82c6263893e0f22df891f8bd8c479ed3f18e69
katkasoft <katkaleks@gmail.com>
2026-02-26 12:28:30 +0300

    time formatting

--- commands/time.c
+++ commands/time.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 
-int main() {
+int main(int argc, char* argv[]) {
     time_t rawtime;
     struct tm *timeinfo;
     time(&rawtime);
     timeinfo = localtime(&rawtime);
-    printf("%s", asctime(timeinfo));
+    if (argc >= 2) {
+        char buffer[80];
+        char format[1024] = "";
+        for (int i = 1; i < argc; i++) {
+            strcat(format, argv[i]);
+        }
+        if (strftime(buffer, sizeof(buffer), format, timeinfo)) {
+            printf("%s\n", buffer);
+        } else {
+            perror("time: error: invalid format");
+        }
+    } else {
+        printf("%s\n", asctime(timeinfo));
+    }
     return 0;
 }
